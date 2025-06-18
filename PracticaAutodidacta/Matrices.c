7e86fa3dfdb32beceef916979260278718ff4e85
Luciano Barberis <barberisluciano58@gmail.com>
2025-06-18 08:04:38 -0300

    Suma y Producto de matrices

--- PracticaAutodidacta/Matrices.c
+++ PracticaAutodidacta/Matrices.c
@@ -3,6 +3,8 @@
 #define T 3
 
 void printMatriz(int mtz[T][T]);
+void sumarMatriz(int mtz1[T][T], int mtz2[T][T], int result[T][T]);
+void productMatriz3(int mtz1[T][T], int mtz2[T][T], int result[T][T]);
 
 int main(){
     int matriz1[T][T];
@@ -10,21 +12,23 @@ int main(){
     int result[T][T];
     int i, j;
 
+    // ? Recorremos y le damos valores a las matrices
     for (i = 0; i < T; i++){
         for (j = 0; j < T; j++){
             matriz1[i][j] = i + j;
-            matriz2[i][j] = i + j;
+            matriz2[i][j] = (i + j) * 2;
         }
     }
+
+    printf("---Matriz 1---\n");
     printMatriz(matriz1);
-    printf("\n");
+    printf("\n---Matriz 2---\n");
     printMatriz(matriz2);
-    for (i = 0; i < T; i++){
-        for (j = 0; j < T; j++){
-            result[i][j] = matriz1[i][j] + matriz2[i][j];
-        }
-    }
-    printf("\n");
+    printf("\n---Matrices Sumadas---\n");
+    sumarMatriz(matriz1, matriz2, result);
+    printMatriz(result);
+    printf("\n---Producto de Matrices---\n");
+    productMatriz3(matriz1, matriz2, result);
     printMatriz(result);
     return 0;
 }
@@ -35,5 +39,27 @@ void printMatriz(int mtz[T][T]) {
             printf("%d ", mtz[i][j]);
         }
         printf("\n");
+    }
+}
+
+void sumarMatriz(int mtz1[T][T], int mtz2[T][T], int result[T][T]) {
+    int i, j;
+    for (i = 0; i < T; i++){
+        for (j = 0; j < T; j++){
+            result[i][j] = mtz1[i][j] + mtz2[i][j];
+        }
+    }
+}
+
+void productMatriz3(int mtz1[T][T], int mtz2[T][T], int result[T][T]) {
+    int i, j, k;
+
+    for (i = 0; i < T; i++){
+        for (j = 0; j < T; j++){
+            result[i][j] = 0;
+            for (k = 0; k < T; k++){
+                result[i][j] += mtz1[i][k] * mtz2[k][j]; 
+            }
+        }
     }
 }
