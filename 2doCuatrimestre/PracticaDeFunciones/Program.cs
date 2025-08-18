d1aeca40814185f3d8449d3d9fd80f9eabfe8da0
Luciano Barberis <barberisluciano58@gmail.com>
2025-08-18 11:16:25 -0300

    Add Adelanto Repaso Final en C#

---
+++ 2doCuatrimestre/PracticaDeFunciones/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace PracticaDeFunciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int personas = 0;
+            Console.WriteLine("Ingrese la cantidad de personas: ");
+            personas = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[personas];
+            string[] nombres = new string[personas];
+
+            for (int i = 0; i < personas; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Ingrese el nombre de la persona {i+1}: ");
+                    nombres[i] = Console.ReadLine();
+                }while (string.IsNullOrWhiteSpace(nombres[i]));
+                do
+                {
+                    Console.WriteLine($"Ingrese la edad de la persona {i + 1}: ");
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+                while (arr[i] < 0);
+            }
+
+            int indice = 0;
+            foreach (var nombre in nombres)
+            {
+                Console.WriteLine($"Lista de invitados {nombre} | Edad: {arr[indice]}");
+                indice++;
+            }
+        }
+    }
+}
---
+++ 2doCuatrimestre/RepasoFinal/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace RepasoFinal
+{
+    internal class Program
+    {
+        const int PAC = 3;
+        const int PRU = 5;
+
+        static void Main(string[] args)
+        {
+            int[,] resultados = new int[PAC, PRU] {
+                {10, 15, 45, 20, 10},  // Dentro de los rangos
+                {12, 22, 50, 30, 8},   // Dentro de los rangos
+                {2, 35, 66, 45, 35}    // Extremos válidos (mínimos o máximos)
+            };
+
+            int[] maxResults = new int[PRU] {15, 25, 60 , 35, 25 };
+            int[] minResults = new int[PRU] {5, 10, 20, 10, 5 };
+
+            // CargarResultados(resultados);
+            detectValorAlto(resultados, maxResults);
+            detectValorMinimo(resultados, minResults);
+        }
+
+        static void CargarResultados(int[,] resultados)
+        {
+            for (int i = 0; i < PAC; i++)
+            {
+                Console.WriteLine($"--- Ingreso de resultados del paciente {i + 1} ---");
+                for (int j = 0; j < PRU; j++)
+                {
+                    int resultado;
+                    do
+                    {
+                        Console.WriteLine($"Ingrese el resultado de la prueba {j + 1}: ");
+                        resultado = int.Parse(Console.ReadLine());
+                    } while (resultado < 0);
+                    resultados[i, j] = resultado;
+                }
+            }
+        }
+
+        static void DetectValorAlto(int[,] resultados, int[] maximos)
+        {    
+            int pacValorAltos = 0;
+
+            Console.WriteLine("--- Valores altos detectados ---");
+
+            for (int i = 0;i < PAC; i++)
+            {
+                for (int j = 0; j < PRU; j++)
+                {
+                    if (resultados[i,j] > maximos[j])
+                    {
+                        pacValorAltos++;
+                    }
+                }
+            }
+            Console.WriteLine($"Los valores altos detectados fueron: {pacValorAltos}");
+        }
+
+        static void DetectValorMinimo(int[,] resultados, int[] minimos) 
+        {
+            int pacValorBajo = 0;
+
+            Console.WriteLine("--- Valores minimos detectados ---");
+            for (int i = 0; i < PAC; i++)
+            {
+                for (int j = 0; j < PRU; j++)
+                {
+                    if (resultados[i, j] > minimos[j])
+                    {
+                        pacValorBajo++;
+                    }
+                }
+            }
+            Console.WriteLine($"Los valores bajos detectados fueron: {pacValorBajo}");
+        }
+
+        static void ResultadosAnormales(int[,] resultados, int[] maximos, int[] minimos)
+        {
+            Console.WriteLine("--- Resultados Anormales ---");
+        }
+
+        static void CalcPromEspecifca()
+        {
+            Console.WriteLine("--- Promedio prueba especifica ---");
+        }
+
+    }
+}
